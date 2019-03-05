cd4a0d2d162c9e515955dc198fcc6f23109a2616
Federico Maestà <f.maesta@softjam.it>
2019-03-05 14:25:53 +0100

    Add script to generate CSV file for O365 configuration

---
+++ prepareCSV/createCsvFileforO365fromLotusExtractor.ps1
@@ -0,0 +1,25 @@
+#Stagin Version 0.1
+
+Param (
+    [Parameter(Position=0,Mandatory=$true)]
+    [string]$file   
+
+)
+
+$arrForLic = @()
+
+$filecontent = get-content $file
+$filecontent[0] = $filecontent[0]  -replace ' ', ''
+$filecontent | Set-content "tempfile.csv"
+
+$users = Import-Csv "tempfile.csv"
+
+foreach ($user in $users) {
+   $arrForLic += $user.DestinationEmail.ToLower()
+
+}
+
+
+Remove-Item "tempfile.csv"
+
+$arrForLic | Set-Content "usersO365Lic.csv"
