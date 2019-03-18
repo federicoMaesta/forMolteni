187f9f07f806212a730a218cdb8f1d5817ec6568
federicoMaesta <40424709+federicoMaesta@users.noreply.github.com>
2019-03-18 16:39:23 +0100

    apply a lot of field

--- configureExhcangeOnline/configureLangAndTimeZone.ps1
+++ configureExhcangeOnline/configureLangAndTimeZone.ps1
@@ -1,13 +1,19 @@
-#Stagin Version 0.1
-
 Param (
     [Parameter(Position=0,Mandatory=$true)]
     [string]$file   
 
 )
 
-$users = Import-Csv $file
+$LiveCred = Get-Credential
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell/ -Credential $LiveCred -Authentication Basic -AllowRedirection
+Import-PSSession $Session -AllowClobber
+
+Connect-MsolService -Credential $LiveCred
+
+#Stagin Version 0.1
+
+$users = get-content $file
 
 foreach ($user in $users) {
-    Get-Mailbox -Identity $user | Set-MailboxRegionalConfiguration  -Language it-IT -TimeFormat HH:mm -TimeZone "W. Europe Standard Time" -Mailbo-LocalizeDefaultFolderName:$true
+    Get-Mailbox -Identity $user | Set-MailboxRegionalConfiguration  -Language it-IT -TimeFormat HH:mm -TimeZone "W. Europe Standard Time"
 }
